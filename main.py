c67823161568f0c0b51c6c4a2f6298870e8dade1
Jhoiker0024 <jhoikerfagundez2002@gmail.com>
2026-03-02 22:51:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.markdown(
     <style>
     .stApp {
         background-image: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)),
-        url("https://drive.google.com/uc?export=view&id=1OKOn2U69T_U4eVVUgK_dXbgyt8EsLAl3.jpg");
+        url("https://drive.google.com/uc?export=view&id=1OKOn2U69T_U4eVVUgK_dXbgyt8EsLAl3");
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
